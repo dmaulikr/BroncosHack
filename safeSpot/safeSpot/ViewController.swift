ee437afa2acfddf0b053783aa5b3daf4eb2b6b7f
Jessie Albarian <albarian.jess@gmail.com>
2016-11-18 23:10:01 -0700

    added map

--- safeSpot/safeSpot/ViewController.swift
+++ safeSpot/safeSpot/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mapView?.mapType = .Standard
+        
+        let nyc = CLLocationCoordinate2D(latitude: 40.7326808, longitude: -73.9843407)
+        mapView?.setCenterCoordinate(nyc, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
