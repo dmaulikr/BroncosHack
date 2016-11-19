5c4f777b87186723378c6b3597502a778c91869c
Jessie Albarian <albarian.jess@gmail.com>
2016-11-18 23:17:13 -0700

    more map stuff

--- safeSpot/safeSpot/ViewController.swift
+++ safeSpot/safeSpot/ViewController.swift
@@ -17,9 +17,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         mapView?.mapType = .Standard
-        
-        let nyc = CLLocationCoordinate2D(latitude: 40.7326808, longitude: -73.9843407)
-        mapView?.setCenterCoordinate(nyc, animated: true)
+        let span = MKCoordinateSpanMake(0.1, 0.1)
+        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 40.7326808, longitude: -73.9843407), span: span)
+        mapView.setRegion(region, animated: true)
+//        let nyc = CLLocationCoordinate2D(latitude: 40.7326808, longitude: -73.9843407)
+//        mapView?.setCenterCoordinate(nyc, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
