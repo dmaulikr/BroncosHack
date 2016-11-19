35370c752a16bb2c6f56bfb83c533a03df686809
Jessie Albarian <albarian.jess@gmail.com>
2016-11-19 09:53:43 -0700

    added second project to test

---
+++ SafeSpot2/SafeSpot2Tests/SafeSpot2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  SafeSpot2Tests.swift
+//  SafeSpot2Tests
+//
+//  Created by Jessie Albarian on 11/19/16.
+//  Copyright © 2016 teamawesomesauce. All rights reserved.
+//
+
+import XCTest
+@testable import SafeSpot2
+
+class SafeSpot2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
--- safeSpot/safeSpot/LoginViewController.swift
+++ safeSpot/safeSpot/LoginViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Lock
+//import Auth0
 
 class LoginViewController: UIViewController {
 
@@ -15,36 +16,52 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-
-    }
-    
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(false)
         let controller = A0Lock.sharedLock().newLockViewController()
-        controller.closable = false
+        controller.closable = true
         controller.onAuthenticationBlock = { profile, token in
             // Do something with token  profile. e.g.: save them.
             // Lock will not save these objects for you.
             
-//            self.usernameLabel.text = profile.name
-//            self.emailLabel.text = profile.email
-//            controller.sharedInstance.accessToken = token.accessToken
-            
             // Don't forget to dismiss the Lock controller
-            print("profile: ", profile, " token:", token)
-            // MyApplication.sharedInstance.accessToken = token.accessToken
-//            controller.dismissViewControllerAnimated(true, completion: nil)
-            
-//            let VC1 = self.storyboard?.instantiateViewControllerWithIdentifier("SafeSpot") as! ViewController
-//            let navController = UINavigationController(rootViewController: VC1) // Creating a navigation controller with VC1 at the root of the navigation stack.
-//            self.presentViewController(navController, animated:true, completion: nil)
- 
-            let secondViewController = self.storyboard!.instantiateViewControllerWithIdentifier("SafeSpot") as! ViewController
-            self.navigationController!.pushViewController(secondViewController, animated: true)
-            
+            controller.dismissViewControllerAnimated(true, completion: nil)
         }
         A0Lock.sharedLock().presentLockController(controller, fromController: self)
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(false)
+        
+//        let controller = A0Lock.sharedLock().newLockViewController()
+//        controller.closable = true
+//        controller.onAuthenticationBlock = { profile, token in
+//            // Do something with token  profile. e.g.: save them.
+//            // Lock will not save these objects for you.
+//            
+////            self.usernameLabel.text = profile.name
+////            self.emailLabel.text = profile.email
+////            controller.sharedInstance.accessToken = token.accessToken
+//            
+//            // Don't forget to dismiss the Lock controller
+//            print("profile: ", profile, " token:", token)
+////            .sharedInstance.accessToken = token.accessToken
+//            
+//            controller.dismissViewControllerAnimated(true, completion: nil)
+//            
+////            let VC1 = self.storyboard?.instantiateViewControllerWithIdentifier("SafeSpot") as! ViewController
+////            let navController = UINavigationController(rootViewController: VC1) // Creating a navigation controller with VC1 at the root of the navigation stack.
+////            self.presentViewController(navController, animated:true, completion: nil)
+// 
+////            let secondViewController = self.storyboard!.instantiateViewControllerWithIdentifier("SafeSpot") as! ViewController
+////            self.navigationController!.pushViewController(secondViewController, animated: true)
+//            
+//        }
+//        
+//        A0Lock.sharedLock().presentLockController(controller, fromController: self)
+        
+        
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
