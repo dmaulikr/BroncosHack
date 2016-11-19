a008eebc64e0d3b3445a123debc37d822b014a8d
Jessie Albarian <albarian.jess@gmail.com>
2016-11-19 04:39:21 -0700

    Integrated pod dependencies for implementing Auth0

---
+++ safeSpot/Pods/Target Support Files/Auth0/Auth0-umbrella.h
@@ -0,0 +1,10 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "Auth0.h"
+#import "A0ChallengeGenerator.h"
+
+FOUNDATION_EXPORT double Auth0VersionNumber;
+FOUNDATION_EXPORT const unsigned char Auth0VersionString[];
+
